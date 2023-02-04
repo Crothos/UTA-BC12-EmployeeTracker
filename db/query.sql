5bcdea1d8d6f30b8b82373f3c7b5e5b9493ba3a9
Crothos <s.g.totten@gmail.com>
2023-02-03 23:20:24 -0600

    View Alls Work

--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,14 @@
 SELECT * FROM employee;
+
+-- View All Employees
+SELECT a.id,
+    a.first_name AS "First Name",
+    a.last_name AS "Last Name",
+    roles.title AS "Title",
+    department.dept_name AS "Department",
+    roles.salary AS "Salary",
+    CONCAT(b.first_name, " ", b.last_name) AS "Manager"
+FROM employee AS a
+JOIN roles ON a.role_id = roles.id
+JOIN department ON roles.department_id = department.id
+LEFT OUTER JOIN employee AS b ON a.manager_id = b.id;
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
+// Connection Info
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
 const conTable = require("console.table")
 
-const db = mysql.createConnection(
+const db = mysql.createConnection (
   {
     host: 'localhost',
     user: 'root',
@@ -13,6 +13,7 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee database.`)
 );
 
+// Primary Prompt
 const promptUser = () => {
     inquirer.prompt([
       {
@@ -60,9 +61,45 @@ const promptUser = () => {
 }
 
 
+function displayDepartments() {
+  db.query('SELECT id, name AS "Name" FROM employee_db.department', function (err, results) {
+    console.table(results);
+    promptUser();
+  });
+}
 
+function displayRoles() {
+  db.query(`SELECT role.id,
+            role.title,
+            department.name AS "Department",
+            role.salary FROM employee_db.role
+        JOIN department ON role.department_id = department.id;
+    `, function (err, results) {
+    console.table(results);
+    promptUser();
+  });
+}
 
+function displayEmployees() {
+  db.query(`
+  SELECT a.id,
+    a.first_name AS "First Name",
+    a.last_name AS "Last Name",
+    role.title AS "Title",
+    department.name AS "Department",
+    role.salary AS "Salary",
+    CONCAT(b.first_name, " ", b.last_name) AS "Manager"
+    FROM employee AS a
+    JOIN role ON a.role_id = role.id
+    JOIN department ON role.department_id = department.id
+    LEFT OUTER JOIN employee AS b ON a.manager_id = b.id;
+  `, function (err, results) {
+    console.table(results);
+    promptUser();
+  });
+}
 
+// Function to display hello on initialize.
  const init = () => {
     console.log("///////////////////////////////////"),
     console.log("/                                 /"),
@@ -73,8 +110,7 @@ const promptUser = () => {
     promptUser()
     };
 
-init();
-
+// Function to display goodbye on quit.
 const goodBye = () => {
   console.log("///////////////////////////////////"),
   console.log("/                                 /"),
@@ -82,3 +118,6 @@ const goodBye = () => {
   console.log("/                                 /"),
   console.log("///////////////////////////////////")
 };
+
+// Initialize function call
+init();
