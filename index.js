a9aa248a86962e77fea0824fab36d6e1ae3a9f27
Stephen Totten <s.g.totten@gmail.com>
2023-02-05 00:56:50 -0600

    Updated index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection (
   {
     host: 'localhost',
     user: 'root',
-    password: '*********',
+    password: 'AngryCuddle5',
     database: 'employee_db'
   },
   console.log(`Connected to the employee database.`)
